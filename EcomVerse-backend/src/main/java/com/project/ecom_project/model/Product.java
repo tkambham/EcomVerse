4386aa37d030a48f40580152e1a36c3324a36462
Trilok Kambham <157092338+tkambham@users.noreply.github.com>
2025-09-22 14:15:35 -0500

    Add files via upload

---
+++ EcomVerse-backend/src/main/java/com/project/ecom_project/model/Product.java
@@ -0,0 +1,61 @@
+package com.project.ecom_project.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigInteger price;
+    private String category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy")
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
+}
---
+++ EcomVerse-backend/src/main/java/com/project/ecom_project/service/ProductService.java
@@ -0,0 +1,54 @@
+package com.project.ecom_project.service;
+
+import com.project.ecom_project.model.Product;
+import com.project.ecom_project.repo.ProductRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product prod, MultipartFile imageFile) throws IOException {
+        prod.setImageName(imageFile.getOriginalFilename());
+        prod.setImageType(imageFile.getContentType());
+        prod.setImageData(imageFile.getBytes());
+        System.out.println(prod);
+        return repo.save(prod);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageData(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repo.save(product);
+    }
+
+    public void deleteProductById(int id) {
+        try{
+            repo.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
+    }
+}
